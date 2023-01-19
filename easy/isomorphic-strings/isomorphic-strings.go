4e5fcfe6180270ca4634c17c93bb8baea4e55b9b
Hieu Le Dinh Trung <hieu.tdle@gmail.com>
2023-01-19 23:03:25 +0100

    update

---
+++ easy/isomorphic-strings/isomorphic-strings.go
@@ -0,0 +1,38 @@
+package isomorphic_strings
+
+func isIsomorphic(s string, t string) bool {
+
+	// Store the length of the first string
+	l := len(s)
+
+	// If the length of the two strings are not equal, return false
+	if l != len(t) {
+		return false
+	}
+
+	// Create a map to store the mapping of the characters
+	m := make(map[byte]byte)
+
+	// Second map to calculate the length of unique characters from the second strings
+	m2 := make(map[byte]bool)
+
+	for i := 0; i < l; i++ {
+		// If the character is not present in the map, add it, if it exists, check if the value is same as previous one.
+		if _, ok := m[s[i]]; !ok {
+			m[s[i]] = t[i]
+		} else if m[s[i]] != t[i] {
+			return false
+		}
+		m2[t[i]] = true
+	}
+
+	// If the length of the map is not equal to the length of the second string
+	// Mean unique characters are not equal between two strings
+	// Return false
+	if len(m) != len(m2) {
+		return false
+	}
+
+	return true
+
+}
---
+++ easy/isomorphic-strings/isomorphic-strings_test.go
@@ -0,0 +1,44 @@
+package isomorphic_strings
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// testcases
+var tcs = []struct {
+	s   string
+	t   string
+	ans bool
+}{
+
+	{"ba", "a", false},
+	{"a", "a", true},
+	{"aba", "baa", false},
+	{"ab", "aa", false},
+	{"egg", "add", true},
+	{"foo", "bar", false},
+	{"paper", "title", true},
+}
+
+func Test_isIsomorphic(t *testing.T) {
+
+	ast := assert.New(t)
+
+	fmt.Printf("------------------------Leetcode Problem 205. Isomorphic Strings------------------------\n")
+
+	for _, tc := range tcs {
+		fmt.Printf("【Input】: t = %v s = %v\n【Output】: %v \n", tc.t, tc.s, isIsomorphic(tc.s, tc.t))
+		ast.Equal(tc.ans, isIsomorphic(tc.s, tc.t), "Case: %v", tc)
+	}
+	fmt.Printf("\n\n\n")
+}
+
+func Benchmark_isIsomorphic(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, tc := range tcs {
+			isIsomorphic(tc.s, tc.t)
+		}
+	}
+}
