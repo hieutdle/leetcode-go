c4ced973ffc687a247422bef1a831cca4b460b96
Hieu Le Dinh Trung <dinhtrunghieu.le@student.hpi.uni-potsdam.de>
2023-02-16 23:44:39 +0100

    Create maximum-depth-of-binary-tree.go

---
+++ algorithms/easy/maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.go
@@ -0,0 +1,20 @@
+package algorithms
+
+import "github.com/goldennovember/leetcode-go/gods"
+
+type TreeNode = gods.TreeNode
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
